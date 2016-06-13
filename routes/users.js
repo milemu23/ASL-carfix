98c22bf044e988409948e6425a969cda0dc8e8b1
milemu23 <mlmunksgard@fullsail.edu>
2016-06-12 23:13:06 -0400

    Register auth

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,33 @@ router.get('/', function(req, res, next) {
 
 //set route for register
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register', {title: 'Register'});
+});
+
+//set route for login
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
+//post request to register
+router.post('/register', function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  //Form validator
+  req.checkBody('name', 'Name field is required').notEmpty();
+
+  //Check Errors
+  var errors = req.validationErrors();
+
+  if(errors){
+    console.log('Errors');
+  } else{
+    console.log('No Errors');
+  }
 });
 
 module.exports = router;
